a2025719d7fb44da7e4cd15c030b3a1724b52e14
Mike Garrity <mike@rustytriangles.com>
2020-09-04 10:03:43 -0400

    Updates

---
+++ precache-manifest.04c29d8571bb598abd3ffeef507f7472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9ea08df3dad9f94c076b414e22a8ef",
+    "url": "/r3f-teapot/index.html"
+  },
+  {
+    "revision": "aae06998333903497dfe",
+    "url": "/r3f-teapot/static/css/main.e9f8d862.chunk.css"
+  },
+  {
+    "revision": "03d7ab22b03eb3243c3e",
+    "url": "/r3f-teapot/static/js/2.3a335c88.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/r3f-teapot/static/js/2.3a335c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae06998333903497dfe",
+    "url": "/r3f-teapot/static/js/main.e5303f36.chunk.js"
+  },
+  {
+    "revision": "1450177ee4e9b86554f1",
+    "url": "/r3f-teapot/static/js/runtime-main.40b0a8fa.js"
+  }
+]);
